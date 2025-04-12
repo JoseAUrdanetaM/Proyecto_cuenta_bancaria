c22133279ceb1e9e05a454048a826cb7f5a83202
Jose <urdaneta.j@outlook.com>
2025-04-11 20:09:46 -0500

    Add project files.

---
+++ Proyecto_cuenta_bancaria/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Proyecto_cuenta_bancaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
